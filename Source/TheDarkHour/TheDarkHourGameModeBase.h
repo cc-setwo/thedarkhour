9825b4190a3da61e220d3daf0a13b431030e146e
CC <cc.setwo@gmail.com>
2017-02-12 20:12:12 +0200

    Initial commit

---
+++ Source/TheDarkHour/TheDarkHourGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TheDarkHourGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEDARKHOUR_API ATheDarkHourGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
